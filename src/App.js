a95dcd323a91c67783f3b102606fd7c2959d816f
Koopabro <ehotamis@gmail.com>
2021-02-18 22:07:36 +0100

    slight refactor

--- src/App.js
+++ src/App.js
@@ -6,49 +6,20 @@ import {
   Text,
   VStack,
   HStack,
-  Grid,
-  theme,
-  Button,
   Image,
+  Heading,
+  Button,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react';
 import jvg from './jvg.png';
-import Uitleg from './uitleg'
+import Stellingen from './Stelling';
 //TODO: Cijfertjes aan de bovenkant van de box groen/rood/neutraal zodat je terug kan gaan
 
 function App() {
-  const Stellingen = [
-    'Accijnzen voor tabak moeten omhoog.',
-    'Alleen sigarenboeren/gespecialiseerde zaken mogen nog tabaksproducten verkopen.',
-    'Roken in openbare ruimtes is niet meer toegestaan.',
-    'Alle voedingsmiddelen krijgen verplicht een gezondheidslabel.',
-    'Scholen mogen geen suikerhoudende dranken en ongezonde voeding verkopen.',
-    'De suikertaks wordt ingevoerd voor minimaal alle suikerhoudende dranken.',
-    'Accijnzen voor alcoholhoudende dranken moeten omhoog.',
-    'Alcoholreclame mag pas na 00.00 uur worden uitgezonden.',
-    'Zwemlessen worden weer een verplicht onderdeel op basisscholen.',
-    'Sporten wordt gratis voor huishoudens met laag inkomen.',
-    'Huisvesting van niet-zelfredzame ouderen moet prioriteit krijgen in het bouwprogramma.',
-    'Mantelzorgers krijgen meer budget om goede zorg te kunnen verlenen.',
-    'Zorgprofessionals krijgen beter uitbetaald.',
-    'Er wordt een maximum gesteld aan wat een arts mag verdienen.',
-    'Artsen moeten in loondienst komen van de zorginstelling waar ze werken.',
-    'Marktwerking in de zorg wordt afgeschaft.',
-    'Het eigen risico wordt komende vier jaar afgebouwd.',
-    'Medicijnen moeten door Nederlandse bedrijven worden geproduceerd.',
-    'Farmaceutische bedrijven mogen niet meer zoveel winst maken.',
-    'De overheid moet meer investeren in preventie en zou dit moeten financieren uit de basiszorg.',
-    'Basisscholen worden verplicht te onderwijzen over en te ondersteunen in gezonde leefstijlkeuzes.',
-    'Een vaccinatieplicht moet worden ingevoerd voor het COVID-19 virus.',
-    'Mensen die hun leven als voltooid zien, hebben het recht op euthanasie.',
-    'Vrouwen moeten de abortuspil bij hun huisarts kunnen halen.',
-    'Vrouwen bepalen zelf wanneer zij in de zwangerschap voor een abortus kiezen.',
-    'Er moet meer medisch-wetenschappelijk onderzoek naar voortplantingstechnieken en het voorkomen van ernstige genetische afwijkingen.',
-    'Er wordt meer geïnvesteerd in geestelijke gezondheidszorg op wijkniveau.',
-    'Verantwoordelijkheid voor jeugdzorg komt bij de Rijksoverheid te liggen.',
-    'Gezondheid van mensen moet centraal komen te staan in klimaatbeleid.',
-    'Om toekomstige pandemieën door zoönosen (ziektes vanuit dieren) te voorkomen, moet grootschalige veehouderij en fokkerij worden aangepakt.',
-  ];
-
   const [antwoord, setAntwoord] = useState([]);
   const [stelling, setStelling] = useState(0);
 
@@ -63,69 +34,105 @@ function App() {
   };
 
   return (
-    <ChakraProvider theme={theme}>
-      <Box bg="#FFC03A" textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack spacing={12}>
-            <Image src={jvg} />
-            <Box p={12} bg="white">
-              <HStack >
-                {stelling < 31
-                  ? Stellingen.map((c, i) => (
-                      <button onClick={() => setStelling(i + 1)}>
-                        {i + 1}
-                      </button>
-                    ))
-                  : null}
-              </HStack>
-              <VStack spacing={12}>
-                <Box>
-                  <Text fontSize="3xl">
-                  <h1>
-                    <strong>
-                      {' '}
-                      {/* titel */}
-                      {stelling === 0
-                        ? 'Kieswijzer Zorg'
-                        : stelling > 30
-                        ? 'Uitslag'
-                        : `Stelling ${stelling}`}
-                    </strong>
-                  </h1>{' '}</Text>
-                  {/* inhoud */}
-                  {stelling === 0
-                    ? 'Wat denken politieke partijen over de zorg?'
-                    : <Box><Text>{Stellingen[stelling - 1]}</Text></Box>}
-                  {stelling > 30 && (
-                    <div>
-                      Dit zijn je resultaten voor de verkiezingen:
-                      <VStack>
-                        {data.map(x =>  
-                          <Box>
-                            {x.partij}: {matchParty(x.wijzer.map(y => y.antw), antwoord.map(z => z.antw))}%
-                          </Box>
+    <ChakraProvider>
+      <Box minH="100vh" bg="#FFC03A">
+        <VStack spacing={12}>
+          <Image src={jvg} />
+          <Box bg="white" w="3xl" maxW="100%" textAlign="center" p={8}>
+            <VStack spacing={8}>
+              {/* Voor wie dit leest, I'm sorry voor deze ongelovelijke warboel*/}
+
+              {/* Headings*/}
+              {stelling === 0 && <Heading>Gezondheidsstemwijzer</Heading>}
+              {stelling < 31 && stelling > 0 && (
+                <Heading>Stelling {stelling}</Heading>
+              )}
+
+              {/* Texten */}
+              {stelling === 0 && (
+                <Text fontSize="xl">
+                  Wat denken politieke partijen over gezondheid?
+                </Text>
+              )}
+
+              {stelling < 31 && stelling > 0 && (
+                <Text fontSize="xl">{Stellingen[stelling - 1]}</Text>
+              )}
+
+              {stelling > 30 && (
+                <>
+                  <Text fontSize="2xl">
+                    Dit zijn je resultaten voor de Gezondheidswijzer.
+                  </Text>
+                  <VStack>
+                    {data.map(x => (
+                      <Box key={x.partij}>
+                        <strong>{x.partij}</strong>:{' '}
+                        {matchParty(
+                          x.wijzer.map(y => y.antw),
+                          antwoord.map(z => z.antw)
                         )}
+                        %
+                      </Box>
+                    ))}
+                  </VStack>
+                </>
+              )}
+
+              {/* Knoppen */}
+              {stelling > 0 && stelling < 31 && (
+                <HStack>
+                  <Button onClick={() => updateAntwoord(0)}>Tegen</Button>
+                  <Button onClick={() => updateAntwoord(1)}>Neutraal</Button>
+                  <Button onClick={() => updateAntwoord(2)}>Voor</Button>
+                </HStack>
+              )}
+              {stelling === 0 && (
+                <Button onClick={() => setStelling(stelling + 1)}>
+                  Doe de kieswijzer →
+                </Button>
+              )}
+
+              {/* Disclaimer */}
+              {stelling === 0 && (
+                <Text fontSize="xs">
+                  Voor deze kieswijzer is contact opgenomen met de partijen die
+                  een zetel peilen, danwel op lokaal niveau een rol hebben
+                  gespeeld. Partijen X, Y en Z hebben schriftelijk hun standpunt
+                  op de stellingen toegelicht. Van de overige partijen hebben we
+                  gekeken wat er in het verkiezingsprogramma stond. De PVV heeft
+                  expliciet aangegeven niet meegenomen te willen worden in deze
+                  stemwijzer.{' '}
+                </Text>
+              )}
+
+              {stelling < 31 && stelling > 0 && (
+                <Accordion>
+                  <AccordionItem>
+                    <AccordionButton>
+                      Dit vinden de partijen: <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel pb={4}>
+                      <VStack spacing={4}>
+                        {data
+                          .map(x => ({
+                            naam: x.partij,
+                            filt: x.wijzer.filter(y => y.stelling === stelling),
+                          }))
+                          .map(ele => (
+                            <Text key={ele.naam}>
+                              <strong>{ele.naam}</strong>:{' '}
+                              {ele.filt[0].toelichting}
+                            </Text>
+                          ))}
                       </VStack>
-                    </div>
-                  )}
-                </Box>{' '}
-                {/* Knoppen */}
-                {stelling === 0 && (
-                  <Button onClick={() => setStelling(stelling + 1)}>
-                    Doe de kieswijzer →
-                  </Button>
-                )}
-                {stelling > 0 && (
-                  <HStack>
-                    <Button onClick={() => updateAntwoord(0)}>Tegen</Button>
-                    <Button onClick={() => updateAntwoord(1)}>Neutraal</Button>
-                    <Button onClick={() => updateAntwoord(2)}>Voor</Button>
-                  </HStack>
-                )}
-              </VStack>
-            </Box>
-          </VStack>
-        </Grid>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              )}
+            </VStack>
+          </Box>
+        </VStack>
       </Box>
     </ChakraProvider>
   );
